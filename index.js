c18c404cdaab9993b5b320ca6592c111344c0f0b
kpatel115 <kp9338448@gmail.com>
2022-08-30 08:51:10 -0500

    updated js to update cost of cart items

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+// add to cart buttons
 let carts = document.querySelectorAll(".add-cart");
-
+ // Products on sales page
 let products = [
     {
         name: "Polo Shirt",
@@ -27,32 +28,73 @@ let products = [
     }
 
 ]
+// for loop counts all added to cart products together
 for (let i=0; i < carts.length; i++) {
     carts[i].addEventListener("click", () => {
-        cartNumbers();
+        cartNumbers(products[i]);
+        totalCost(products[i]);
     })
 }
 
 function onLoadCartNumbers() {
     let productNumbers = localStorage.getItem("cartNumbers");
-
     if(productNumbers) {
         document.querySelector(".cart span").textContent = productNumbers;
     }
 }
 
-function cartNumbers() {
+function cartNumbers(product) {
     let productNumbers = localStorage.getItem("cartNumbers");
-
-
     productNumbers = parseInt(productNumbers);
-
     if( productNumbers ) {
         localStorage.setItem("cartNumbers", productNumbers + 1);
         document.querySelector(".cart span").textContent = productNumbers + 1;
     } else {
         localStorage.setItem("cartNumbers", 1);
         document.querySelector(".cart span").textContent = 1;
+    }
+    setItems(product);
+    
+}
+
+ // allows product that is added to cart to show up as JSON object in local storage
+function setItems(product) {
+    let cartItems = localStorage.getItem("productsInCart");
+    cartItems = JSON.parse(cartItems);
+    console.log("my cartItems are", cartItems);
+
+    if(cartItems != null) {
+
+        if(cartItems[product.tag] == undefined ) {
+            cartItems = {
+                ...cartItems,
+                [product.tag]: product
+            }
+        }
+        cartItems[product.tag].inCart += 1;
+    } else {
+        product.inCart = 1;
+        cartItems = {
+            [product.tag]: product
+        }
+    }
+
+    localStorage.setItem("productsInCart", JSON.stringify
+    (cartItems));
+}
+
+function totalCost(product) {
+    // console.log("The product price is", product.price);
+    let cartCost = localStorage.getItem("totalCost");
+    
+    console.log("my cartCost is", cartCost);
+    console.log(typeof cartCost);
+
+    if(cartCost != null) {
+        cartCost = parseInt(cartCost);
+        localStorage.setItem("totalCost", cartCost + product.price);
+    } else {
+        localStorage.setItem("totalCost", product.price);
     }
 
     
